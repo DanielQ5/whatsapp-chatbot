86b1b5e0569ee899971ef3812ec71dd2aba62845
Daniel Quintanilla <daniel2356work@gmail.com>
2026-02-14 19:06:55 -0600

    Add UserSession service class

---
+++ whatsapp-chatbot/src/main/java/com/chatbot/whatsapp_chatbot/insurance/service/UserSession.java
@@ -0,0 +1,25 @@
+package com.chatbot.whatsapp_chatbot.insurance.service;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class UserSession {
+    private String phoneNumber;
+    private String policyNumber;
+    private List<String> actionsTaken = new ArrayList<>();
+    private boolean requestedRepresentative = false;
+    private LocalDateTime sessionStart = LocalDateTime.now();
+
+    public UserSession(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void addAction(String action) {
+        actionsTaken.add(action);
+    }
+
+}
