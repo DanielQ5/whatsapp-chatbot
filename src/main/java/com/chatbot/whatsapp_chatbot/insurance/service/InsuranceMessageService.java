30229b8dfff5a39f9f46e0d8a9d3d3af1559446a
Daniel Quintanilla <daniel2356work@gmail.com>
2026-02-21 21:08:26 -0600

    Adding handleMenuChoice and registerPolicy methods

--- src/main/java/com/chatbot/whatsapp_chatbot/insurance/service/InsuranceMessageService.java
+++ src/main/java/com/chatbot/whatsapp_chatbot/insurance/service/InsuranceMessageService.java
@@ -2,11 +2,14 @@ package com.chatbot.whatsapp_chatbot.insurance.service;
 
 
 import com.chatbot.whatsapp_chatbot.insurance.chatanalytics.repository.InteractionLogRepository;
+import com.chatbot.whatsapp_chatbot.insurance.production.entity.Policy;
 import com.chatbot.whatsapp_chatbot.insurance.production.repository.PolicyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
@@ -40,26 +43,19 @@ public class InsuranceMessageService {
 
         String response;
 
-       if (cleanMessage.equals("buenos dias") || cleanMessage.equals("hola")){
-           response = showWelcome();
-       }
-       else if (cleanMessage.equals("adios") || cleanMessage.equals("0")){
-           response=endConversation(userSession, phoneNumber);
-       }
-
-       else if (userSession.getPolicyNumber() != null && isMenuOption(messageContent.trim())){
-           response = handleMenuChoice(userSession, messageContent.trim());
-       }
-
-       else if (looksLikePolicyNumber(messageContent.trim().toUpperCase())){
-           response = registerPolicy(userSession, messageContent.trim().toUpperCase());
-       }
-
-       else {
-           response = "Lo sentimos, pero no logrammos entender lo que necesitas.\n\n" +
-                   "Favor ingresa 'hola' para dar inicio o\n" +
-                   "ingresa 'adios' para finalizar";
-       }
+        if (cleanMessage.equals("buenos dias") || cleanMessage.equals("hola")) {
+            response = showWelcome();
+        } else if (cleanMessage.equals("adios") || cleanMessage.equals("0")) {
+            response = endConversation(userSession, phoneNumber);
+        } else if (userSession.getPolicyNumber() != null && isMenuOption(messageContent.trim())) {
+            response = handleMenuChoice(userSession, messageContent.trim());
+        } else if (looksLikePolicyNumber(messageContent.trim().toUpperCase())) {
+            response = registerPolicy(userSession, messageContent.trim().toUpperCase());
+        } else {
+            response = "Lo sentimos, pero no logrammos entender lo que necesitas.\n\n" +
+                    "Favor ingresa 'hola' para dar inicio o\n" +
+                    "ingresa 'adios' para finalizar";
+        }
 
         return response;
     }
@@ -72,11 +68,23 @@ public class InsuranceMessageService {
     private String registerPolicy(UserSession session, String policyNumber) {
         // TODO:
         // 1. Query database: policyRepo.findByPolicyNumber(policyNumber)
+        Optional<Policy> policyOptional = policyRepository.findByPolicyNumber(policyNumber);
+
         // 2. If policy not found, return error message
+        if (policyOptional.isEmpty()) {
+            return "Póliza no encontrada: " + policyNumber + "\n\n" +
+                    "Por favor verifica el número e intenta de nuevo.";
+        }
+
         // 3. If found, save to session: session.setPolicyNumber(policyNumber)
+        Policy policy = policyOptional.get();
+        session.setPolicyNumber(policyNumber);
+
+
         // 4. Return welcome message with showMenu()
 
-        return "TODO: Implement registerPolicy";
+        return " Bienvenido: " + policy.getCustomerFirstName() + " " + policy.getCustomerLastName() + "\n\n" +
+                showMenu();
     }
 
     private String showMenu() {
@@ -95,12 +103,64 @@ public class InsuranceMessageService {
     private String handleMenuChoice(UserSession session, String menuChoice) {
         // TODO:
         // 1. Get policy number from session
+        String policyBeingWorked = session.getPolicyNumber();
         // 2. Query database: policyRepo.findByPolicyNumber(...)
-        // 3. Track action: session.addAction(MENU_TO_ACTION.get(choice))
-        // 4. Use switch/if-else to return appropriate response
-        // 5. Append showMenu() at the end
+        Optional<Policy> policyOptional = policyRepository.findByPolicyNumber(policyBeingWorked);
 
-        return "TODO: Implement handleMenuChoice";
+        if (policyOptional.isEmpty()) {
+            return "Ocurrio un error al cargar la póliza. Por favor intenta de nuevo.";
+        }
+
+        Policy policy = policyOptional.get();
+        // 3. Track action: session.addAction(MENU_TO_ACTION.get(choice))
+        String trackAction = MENU_TO_ACTION.get(menuChoice);
+        session.addAction(trackAction);
+
+        if (menuChoice.equals("8")) {
+            session.setRequestedRepresentative(true);
+        }
+        // 4. Use switch/if-else to return appropriate response
+        String response;
+
+        switch (menuChoice) {
+            case "1": //Policy Type
+                response = "Tipo de poliza: " + policy.getPolicyType();
+                break;
+
+            case "2": //Deductible
+                response = "El Deducible es: " + "$" + policy.getDeductible();
+                break;
+
+            case "3": //Max Coverage
+                response = "El Maximo a cubrir es de: " + "$" + policy.getMaxCoverage();
+                break;
+
+            case "4": //Coverage Start DAte
+                response = "La fecha efectiva de cobertura es: " + policy.getEffectiveDate();
+                break;
+
+            case "5": //Monthly Premium
+                response = "La cuota a cancelar es de: " + "$" + policy.getMonthlyPremium();
+                break;
+
+            case "6": //Payment Cycle
+                response = "La frecuencia de pago es: " + policy.getPaymentCycle();
+                break;
+
+            case "7": //Status
+                response = "La poliza se encuentra " + policy.getPolicyStatus() + " , con fecha de expiracion de: " + policy.getExpirationDate();
+                break;
+
+            case "8": //Ask for a Representative
+                response = "Conectandote con un ejecutivo en estos momentos...";
+                break;
+
+            default:
+                response = "Opción inválida. Por favor elige 1-8.";
+                break;
+        }
+        // 5. Append showMenu() at the end
+        return response + "\n\n" + showMenu();
     }
 
     private String endConversation(UserSession session, String phoneNumber) {
